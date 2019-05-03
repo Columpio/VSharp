88c0280c7a359b9b1338554cbeaa6976febbdb16
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2019-05-03 17:45:53 +0300

    [test] Added test annotations

--- VSharp.Test/Tests/Strings.cs
+++ VSharp.Test/Tests/Strings.cs
@@ -1,10 +1,13 @@
+using NUnit.Framework;
 using System;
 
 namespace VSharp.Test.Tests
 {
+    [TestSvmFixture]
     public sealed class Strings
     {
         // Expecting HeapRef on empty string
+        [TestSvm]
         public static string EmptyString(int n, int m)
         {
             return String.Empty;
